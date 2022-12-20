59e96225446b680a7fccd81088e6dbb33f92a5e8
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-12-20 18:47:58 -0500

    color scheme change, js code cleaned up, mbl menu

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 const submit = document.getElementById('submit');
 const openM = document.getElementById('open');
+const menuIcon =document.querySelector('#open img')
 const menu = document.querySelector('.menu');
 const links = document.getElementById('mobileLinks')
-const closeM = document.getElementById('close');
 
 openM.addEventListener('click', () => {
-    menu.style.height = '100%';
-    menu.style.width = '70vw';
-    menu.style.padding = "1rem 1rem 100% 1rem";
-    closeM.style.opacity = "1";
-    links.style.opacity = "1";
-    links.style.display = 'block'
+    menuIcon.classList.toggle('icon');
+    menu.classList.toggle('openMenu');
+    links.classList.toggle('showLinks');
 });
-closeM.addEventListener('click', () =>{
-    menu.style.height = '0%';
-    menu.style.width = '0vw';
-    menu.style.padding = "0 0 0 0";
-    closeM.style.opacity = "0";
-    links.style.opacity = "0";
-    links.style.display = 'none'
-});
+
 submit.addEventListener('click', (e) => {
     e.preventDefault();
 })
