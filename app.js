fe1d4e384237f0ca4a825d9f6d78a1e7c76b7141
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-12-25 18:06:42 -0500

    dynamic hero section created

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const closeM = document.getElementById('close');
 const menuIcon =document.querySelector('#open img');
 const menu = document.querySelector('.menu');
 const links = document.querySelector('.menu ul');
+const hero = document.querySelector('.hero');
+const heroTitle = document.querySelector('.hero h2');
+const heroPara = document.querySelector('.hero p');
+const heroImg = document.querySelector('.hero .img');
+let count = getRandomInt(2);
 
 openM.addEventListener('click', () => {
     menu.style.width = "70vw";
@@ -29,7 +34,22 @@ closeM.addEventListener('click', () => {
     links.style.opacity = "0";
     openM.style.display = "block";
     closeM.style.display = "none";
-})
+});
+
+if(count == 0){
+    heroTitle.innerHTML = "USER ARTS";
+    heroPara.innerHTML = " A user inputed photo album with four different themes.";
+    heroImg.innerHTML = '<img src="images/UA-MOBILE.svg" alt= "preview-Of-Feature-Site">'
+}
+if(count == 1){
+    heroTitle.innerHTML = "ADVICE GENERATOR";
+    heroPara.innerHTML = " A fun pastime that gets quotes of great advice from an external API";
+    heroImg.innerHTML = '<img src="images/AG-MOBILE.svg" alt= "preview-Of-Feature-Site">'
+}
+function getRandomInt(max) {
+    let num = Math.floor(Math.random() * max)
+    return num;
+  }
 
 submit.addEventListener('click', (e) => {
     if(email.value == ''){
